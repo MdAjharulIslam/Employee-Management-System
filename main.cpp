c8182b21a2074dae504f77afb399c4571aa400c5
Md_Ajharul_Islam <167879829+MdAjharulIslam@users.noreply.github.com>
2024-05-17 20:59:10 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sting.h>
 using namespace std;
 
 string id[100], name[100], address[150], dob[100], mobile_no[100], doj[100], marstatus[100], workd[100], workl[120], ctc[100], socialins[100], email[100];
